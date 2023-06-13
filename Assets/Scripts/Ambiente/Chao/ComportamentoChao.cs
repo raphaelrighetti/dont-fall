f6959b64719e52e96e28e0b0a6be0780d07946c6
raphaelrighetti <raphaelrighettiramos@hotmail.com>
2023-06-13 15:53:20 -0300

    adicionando logica de novas plataformas

--- Assets/Scripts/Ambiente/Chao/ComportamentoChao.cs
+++ Assets/Scripts/Ambiente/Chao/ComportamentoChao.cs
@@ -5,24 +5,52 @@ using TMPro;
 
 public class ComportamentoChao : MonoBehaviour
 {
+    public float ChanceCriacaoPlataforma;
+    private float contadorCriacaoPlataforma;
+    private float contadorCriacaoPlataformaMax;
     private float tempoAtivo;
-    private float[] possiveisTempos = { 5, 10, 15, 20, 25 };
+    private float[] possiveisTempos = { 10, 15, 20, 25, 30 };
+    private BoxCollider boxCollider;
     private TMP_Text textoTempoAtivo;
+    private PlataformasDisponiveis plataformasDisponiveis;
 
     void Start()
     {
+        boxCollider = GetComponent<BoxCollider>();
         textoTempoAtivo = transform.GetChild(0).GetChild(0).gameObject.GetComponent<TMP_Text>();
+        plataformasDisponiveis = GameObject.FindWithTag("Plataformas Disponiveis").GetComponent<PlataformasDisponiveis>();
 
         tempoAtivo = possiveisTempos[Random.Range(0, possiveisTempos.Length)] + 1;
+
+        contadorCriacaoPlataformaMax = tempoAtivo / (tempoAtivo / 2);
 
         AtualizaTextoTempoAtivo();
     }
 
     void Update()
     {
+        contadorCriacaoPlataforma += Time.deltaTime;
+
+        if (contadorCriacaoPlataforma >= contadorCriacaoPlataformaMax)
+        {
+            int numeroAleatorio = Random.Range(0, 101);
+
+            if (numeroAleatorio <= ChanceCriacaoPlataforma)
+            {
+                CriarPlataforma();
+            }
+
+            contadorCriacaoPlataforma = 0;
+        }
+
         tempoAtivo -= Time.deltaTime;
 
         AtualizaTextoTempoAtivo();
+
+        if (tempoAtivo >= 0.5 && tempoAtivo <= 1)
+        {
+            CriarPlataforma();
+        }
 
         if (tempoAtivo <= 0)
         {
@@ -34,5 +62,44 @@ public class ComportamentoChao : MonoBehaviour
     {
         int valorTexto = (int)tempoAtivo;
         textoTempoAtivo.text = valorTexto.ToString();
+    }
+
+    private void CriarPlataforma()
+    {
+        Vector3 posicaoNovaPlataforma = transform.position;
+
+        switch (EscolherDirecaoNovaPlataforma())
+        {
+            case "Frente":
+                posicaoNovaPlataforma.z += boxCollider.bounds.size.z;
+                break;
+            case "Direita":
+                posicaoNovaPlataforma.x += boxCollider.bounds.size.x;
+                break;
+            case "Atrás":
+                posicaoNovaPlataforma.z -= -boxCollider.bounds.size.z;
+                break;
+            case "Esquerda":
+                posicaoNovaPlataforma.z -= -boxCollider.bounds.size.x;
+                break;
+        }
+
+        if (Physics.OverlapBox(posicaoNovaPlataforma, boxCollider.bounds.extents / 2, transform.rotation).Length > 0)
+        {
+            return;
+        }
+
+        GameObject plataformaEscolhida =
+            plataformasDisponiveis.Plataformas[Random.Range(0, plataformasDisponiveis.Plataformas.Length)];
+
+        Instantiate(plataformaEscolhida, posicaoNovaPlataforma, transform.rotation);
+    }
+
+    private string EscolherDirecaoNovaPlataforma()
+    {
+        string[] direcoes = { "Frente", "Direita", "Atrás", "Esquerda" };
+        string direcao = direcoes[Random.Range(0, direcoes.Length)];
+
+        return direcao;
     }
 }
--- Assets/Scripts/Personagens/Inimigos/SpawnerConfig.cs
+++ Assets/Scripts/Personagens/Inimigos/SpawnerConfig.cs
@@ -8,17 +8,24 @@ public class SpawnerConfig : MonoBehaviour
     public float TempoTrocaDificuldade;
     public int ProbabilidadeSpawn;
     private float contadorDificuldade;
+    private StatusJogador statusJogador;
 
     void Start()
     {
         contadorDificuldade = 0;
+
+        statusJogador = GameObject.FindWithTag("Jogador").GetComponent<StatusJogador>();
     }
 
     void Update()
     {
         contadorDificuldade += Time.deltaTime;
 
-        if (contadorDificuldade >= TempoTrocaDificuldade && TempoSpawn >= 0.10F)
+        if (!statusJogador.Vivo)
+        {
+            ProbabilidadeSpawn = 0;
+        }
+        else if (contadorDificuldade >= TempoTrocaDificuldade && TempoSpawn >= 0.10F)
         {
             TempoSpawn -= 0.05F;
             ProbabilidadeSpawn += 3;
